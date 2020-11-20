baa422586a923aa60d8fe985da3f82feb56e2a68
ns3838 <72807027+ns3838@users.noreply.github.com>
2020-11-19 21:13:26 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,8 @@
+f = input.temperature(TemperatureUnit.FAHRENHEIT)
+c = input.temperature(TemperatureUnit.CELSIUS)
+if f>70:
+    light.set_pixel_color(5, light.rgb(255,0,0))
+elif f>40:
+    light.set_all(light.rgb(0,255,0))
+else:
+    light.set_all(light.rgb(0,0,255))
---
+++ main.ts
@@ -0,0 +1,10 @@
+let f = input.temperature(TemperatureUnit.Fahrenheit)
+let c = input.temperature(TemperatureUnit.Celsius)
+if (f > 70) {
+    light.setPixelColor(5, light.rgb(255, 0, 0))
+} else if (f > 40) {
+    light.setAll(light.rgb(0, 255, 0))
+} else {
+    light.setAll(light.rgb(0, 0, 255))
+}
+
